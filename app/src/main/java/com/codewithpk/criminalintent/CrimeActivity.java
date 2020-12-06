29d4a2dc508cd9046200dc5f77499c7716874df4
kranthi pyreddy <kra330@gmail.com>
2020-12-06 02:22:57 -0500

    Part B - Lab 5: Your Second Activity and Fragments-Name refactored

--- app/src/main/java/com/codewithpk/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/codewithpk/criminalintent/CrimeActivity.java
@@ -6,12 +6,12 @@ import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
-public class CheatActivity extends AppCompatActivity {
+public class CrimeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cheat);
+        setContentView(R.layout.activity_crime);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
